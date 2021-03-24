8b1840b0185f098f1a20f5a9a5b6f29a2b26e10b
aiswaryawalter <aiswaryawalter@gmail.com>
2021-03-25 01:22:56 +0530

    Contract specs

--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -5,6 +5,10 @@ const addresses = {
   epnscore: "0xc882dA9660d29c084345083922F8a9292E58787D",
   dai: "0xf80A32A835F79D7787E8a8ee5721D0fEaFd78108",
   aDai: "0xcB1Fe6F440c49E9290c3eb7f158534c2dC374201",
+  rockstar: "0xCD1B6C8DDB9A735AA5961Ab4aa5B513fba23A3a3", //ropsten address
+  batchMintNFT: "0x9A20272669BBAa6254358ba4BB0520fFc813691C", //ropsten address
+  NFTRewards: "0x1f4CA0b2B0541b942A440A3f6673883eEacb2cb3", //ropsten address
+
 };
 
 export default addresses;
--- packages/contracts/src/index.js
+++ packages/contracts/src/index.js
@@ -2,12 +2,19 @@ import erc20Abi from "./abis/erc20";
 import ownableAbi from "./abis/ownable";
 import epnscoreAbi from "./abis/epnscore";
 import daiAbi from "./abis/dai";
+import rockstarAbi from "./abis/Rockstar.json";
+import ec721Abi from "./abis/ERC721.json";
+import NFTRewards from "./abis/NFTRewards.json";
 
 export const abis = {
   erc20: erc20Abi,
   ownable: ownableAbi,
   epnscore: epnscoreAbi,
-  dai: daiAbi
+  dai: daiAbi,
+  rockstar: rockstarAbi,
+  erc721: ec721Abi,
+  NFTRewards: NFTRewards,
+  
 };
 
 export { default as addresses } from "./addresses";
