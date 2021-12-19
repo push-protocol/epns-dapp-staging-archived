b0e83b460bb60be1f38163f3891866af68462e2f
xander <shuaibualexander@gmail.com>
2021-12-19 03:19:45 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bec291bd92d429921c6d119dde5b7b94.js"
+  "/precache-manifest.1e4fda6a4916305551fa5ee8cb12ea1d.js"
 );
 
 self.addEventListener('message', (event) => {
---
+++ static/js/6.bcbac68e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@project/react-app"]=this["webpackJsonp@project/react-app"]||[]).push([[6],{2029:function(p,c){}}]);
+//# sourceMappingURL=6.bcbac68e.chunk.js.map
