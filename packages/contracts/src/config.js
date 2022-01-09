9ee0e91d4c980faaca9587d81d87f89bd24012f1
xander <shuaibualexander@gmail.com>
2022-01-09 01:06:09 +0100

    update infura keys

--- packages/contracts/src/config.js
+++ packages/contracts/src/config.js
@@ -6,7 +6,7 @@ const config = {
         80001 //for mumbai polygon
     ],
     coreContractChain: 42, //the chain id of the network which the core contract relies on
-    coreRPC: "https://kovan.infura.io/v3/TZCWZ8YCQDH4THP54865SDGTG3XXY8ZAQU"
+    coreRPC: "https://kovan.infura.io/v3/4ff53a5254144d988a8318210b56f47a"
 };
 
 export default config;
