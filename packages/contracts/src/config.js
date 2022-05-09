67f6a95c2f6462133b6ca9bf296f54add69f75de
Nilesh Gupta <guptanilesh2312@gmail.com>
2022-05-09 17:20:33 +0530

    Changed addresses and config

--- packages/contracts/src/config.js
+++ packages/contracts/src/config.js
@@ -1,13 +1,12 @@
 // environmental configurations for the dapp for different environments
 const config = {
-    apiUrl: "https://backend-dev.epns.io/apis",
-    allowedNetworks: [
-        42, //for ropsten
-        80001 //for mumbai polygon
-    ],
-    coreContractChain: 42, //the chain id of the network which the core contract relies on
-    coreRPC: "https://kovan.infura.io/v3/4ff53a5254144d988a8318210b56f47a",
-    googleAnalyticsId: "UA-165415629-5"
+	apiUrl: "https://backend-kovan.epns.io/apis", //the right backend URL to be used
+	allowedNetworks: [
+		42, //for ropsten
+		// 80001 //for mumbai polygon
+	],
+	coreContractChain: 42, //the chain id of the network which the core contract relies on
+	coreRPC: "https://kovan.infura.io/v3/4ff53a5254144d988a8318210b56f47a",
 };
 
 export default config;
