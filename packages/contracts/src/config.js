95e7aebd806f3586976c258022aec6ea0a81bc77
Aman <guptaaman200115@gmail.com>
2022-05-10 13:12:34 +0530

    Staging env

--- packages/contracts/src/config.js
+++ packages/contracts/src/config.js
@@ -1,10 +1,10 @@
 // environmental configurations for the dapp for different environments
 const config = {
-    apiUrl: "https://backend-dev.epns.io/apis",
+    apiUrl: "https://backend-kovan.epns.io/apis", //the right backend URL to be used
     toolingApiUrl: "https://tooling.epns.io/apis",
     allowedNetworks: [
         42, //for ropsten
-        80001 //for mumbai polygon
+        // 80001 //for mumbai polygon
     ],
     coreContractChain: 42, //the chain id of the network which the core contract relies on
     coreRPC: "https://kovan.infura.io/v3/4ff53a5254144d988a8318210b56f47a",
