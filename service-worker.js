471bac9b99b0d344fb5e2e27da8d5936cfd118c1
Nilesh Gupta <guptanilesh2312@gmail.com>
2022-09-04 18:47:28 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c45a0131063aaee1b14db885a6fc54c.js"
+  "/precache-manifest.17462428f34f6a95b46d204efc3933d5.js"
 );
 
 self.addEventListener('message', (event) => {
--- static/js/6.11a9dc2f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@project/react-app"]=this["webpackJsonp@project/react-app"]||[]).push([[6],{2280:function(p,c){}}]);
-//# sourceMappingURL=6.11a9dc2f.chunk.js.map
